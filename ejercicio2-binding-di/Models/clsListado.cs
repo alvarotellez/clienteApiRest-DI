1cc210dcc0a290a144b3c1290078d5363e8ae110
alvarotellez <alvaro.tellez.hidalgo@gmail.com>
2016-12-16 14:33:13 +0100

    Falta por hacer el post y el delete

---
+++ ejercicio2-binding-di/Models/clsListado.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+
+namespace ejercicio2_binding_di.Models
+{
+    public class clsListado
+    {
+        //Ruta hacia la Api Rest
+        private Uri miUrl = new Uri("http://apirestdaviduwp.azurewebsites.net/api/persona");
+        public async Task<ObservableCollection<clsPersona>> getListado()
+        {
+            //Lo usabamos cuando no teniamos conexion a la BD
+            //ObservableCollection<clsPersona> lista = new ObservableCollection<clsPersona>();
+
+            //lista.Add(new clsPersona(1, "Alvaro", "Tellez", DateTime.Now, "Moron", "656456782"));
+            //lista.Add(new clsPersona(2, "David", "Benitez", DateTime.Now, "Dos Hermanas", "656456782"));
+            //lista.Add(new clsPersona(3, "Merecedes", "Requena", DateTime.Now, "Sevilla", "656456782"));
+            //lista.Add(new clsPersona(4, "Jose Antonio", "Ruiz", DateTime.Now, "Carmona", "656456782"));
+            //lista.Add(new clsPersona(5, "Daniel", "Leal", DateTime.Now, "Mairena", "656456782"));
+            //lista.Add(new clsPersona(6, "Migue", "Barrera", DateTime.Now, "Sevilla", "656456782"));
+            //lista.Add(new clsPersona(7, "Carlos", "Prieto", DateTime.Now, "Dos Hermanas", "656456782"));
+            //lista.Add(new clsPersona(8, "Adri", "Pol", DateTime.Now, "Moron", "656456782"));
+
+            
+            //Lo usamos para la api rest
+            //Creacion del cliente 
+            HttpClient client = new HttpClient();
+
+            ObservableCollection<clsPersona> lista = new ObservableCollection<clsPersona>();
+            try
+            {
+                //Al hacer el await hay que hacer que el metodo sea Async
+                string respuesta = await client.GetStringAsync(miUrl);
+                client.Dispose();
+
+                //Convertimos el json para pasarlo luego a la lista
+                lista = JsonConvert.DeserializeObject<ObservableCollection<clsPersona>>(respuesta);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            return lista;
+        }
+    }
+}
---
+++ ejercicio2-binding-di/Views/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using ejercicio2_binding_di.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// La plantilla de elemento Página en blanco está documentada en http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace ejercicio2_binding_di
+{
+    /// <summary>
+    /// Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public clsMainPageVM ViewModel { get; }
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.ViewModel = (clsMainPageVM)this.DataContext;
+        }
+
+
+        private void guardar_Click(object sender, RoutedEventArgs e)
+        {
+            this.txtNombre.GetBindingExpression(TextBox.TextProperty).UpdateSource();
+            this.txtApellidos.GetBindingExpression(TextBox.TextProperty).UpdateSource();
+            this.txtApellidos.GetBindingExpression(TextBox.TextProperty).UpdateSource();
+            this.txtFecha.GetBindingExpression(TextBox.TextProperty).UpdateSource();
+            this.txtTelefono.GetBindingExpression(TextBox.TextProperty).UpdateSource();
+            this.txtDireccion.GetBindingExpression(TextBox.TextProperty).UpdateSource();
+        }
+    }
+}
